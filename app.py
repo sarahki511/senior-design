b1e09c6f69ba6804bd5448e4f7afd5f30f1f4bae
Sarah Ki <svki@ucsd.edu>
2020-11-24 13:14:51 -0800

    completed turning basic prototype web frame to code

--- app.py
+++ app.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 # Bootstrap(app)
 @app.route("/") 
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title = "HOME", id = "home")
+
+@app.route("/consult") 
+def consult():
+    return render_template("consult.html", title = "CONSULT", id = "consult")
+
+@app.route("/respect") 
+def respect():
+    return render_template("respect.html", title = "RESPECT", id = "respect")
+
+@app.route("/apples") 
+def apples():
+    return render_template("apples.html", title = "APPLES", id = "apples")
+
+@app.route("/misa") 
+def misa():
+    return render_template("misa.html", title = "MISA", id = "misa")
 
 if __name__ == "__main__":
     app.run(debug=True)
