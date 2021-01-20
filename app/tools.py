3dca09875903486be15506d1f8bbd4fe6a6366e9
Sarah Ki <svki@ucsd.edu>
2021-01-19 17:17:33 -0800

    rm *.json .DS_Store

---
+++ app/tools.py
@@ -0,0 +1,6 @@
+from app import app
+import subprocess
+import os
+import smtplib
+
+# keeps 
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+from flask import Flask
 from app import app
 
+app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
