ce6198415e7324b55305037eaee931d38a6aff5f
Sarah Ki <svki@ucsd.edu>
2021-01-21 17:23:59 -0800

    added form using bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,7 @@ app.config['SECRET_KEY'] = '\xd8\xf1\xa5\xdd\x8eD\xf7\xdf]\xe7\x05\xf79\xa3\x0e\
 app.config['ALLOWED_EXTENSIONS'] = {'txt', 'csv'}
 app.config['EMAIL_ID'] = os.environ.get('EMAIL_ID')
 app.config['EMAIL_PWD'] = os.environ.get('EMAIL_PWD')
+app.config['IMAGE_UPLOADS'] = 'app/static/image'
+app.config['IMAGE_DOWNLOADS'] = 'app/static/uploads'
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,16 +1,19 @@
 from flask import Flask, redirect, url_for, render_template
 from app import forms
+from app import tools
 from werkzeug.utils import secure_filename
 import os
 from app import app
 from flask.helpers import flash
+import shutil
+import smtplib
 # import locAL3 as lc3
 # import xAndyPlot as xyPlot
 
 
-app.config['SECRET_KEY'] = '\xd8\xf1\xa5\xdd\x8eD\xf7\xdf]\xe7\x05\xf79\xa3\x0e\xd1'
-uploads_dir = os.path.join(app.instance_path, 'uploads')
-os.makedirs(uploads_dir, exist_ok=True)
+# app.config['SECRET_KEY'] = '\xd8\xf1\xa5\xdd\x8eD\xf7\xdf]\xe7\x05\xf79\xa3\x0e\xd1'
+# uploads_dir = os.path.join(app.instance_path, 'uploads')
+# os.makedirs(uploads_dir, exist_ok=True)
 
 # Bootstrap(app)
 @app.route("/") 
@@ -19,27 +22,27 @@ def home():
 
 @app.route("/consult", methods=('GET', 'POST')) 
 def consult():
-    return getForm("consult")
-    # return render_template("consult.html", title = "CONSULT", id = "consult")
+    # return getForm("consult")
+    return render_template("consult.html", title = "CONSULT", id = "consult")
     
 
 @app.route("/respect", methods=('GET', 'POST')) 
 def respect():
-    return getForm("respect")
-    return render_template("respect.html", title = "RESPECT", id = "respect", form = form)
+    # return getForm("respect")
+    return render_template("respect.html", title = "RESPECT", id = "respect")
     
 
 @app.route("/apples", methods=('GET', 'POST')) 
 def apples():
-    return getForm("apples")
-    # return render_template("apples.html", title = "APPLES", id = "apples")
+    # return getForm("apples")
+    return render_template("apples.html", title = "APPLES", id = "apples")
 
 @app.route("/misa", methods=('GET', 'POST')) 
 def misa():
-    return getForm("misa")
-    # return render_template("misa.html", title = "MISA", id = "misa")
+    # return getForm("misa")
+    return render_template("misa.html", title = "MISA", id = "misa")
 
-@app.route("/pending") 
+@app.route("/pending", methods = ('GET', 'POST')) 
 def pending():
     return render_template("pending.html", title = "PENDING", id = "pending")
 
