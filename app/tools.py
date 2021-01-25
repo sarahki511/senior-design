5ab26360f8191cf5db9777b891962cfa5dfc5116
Sarah Ki <svki@ucsd.edu>
2021-01-25 15:13:47 -0800

    finished setting up email

--- app/tools.py
+++ app/tools.py
@@ -1,6 +1,20 @@
 from app import app
 import subprocess
 import os
-import smtplib
+import smtplib, ssl, certifi
 
-# keeps 
+def sendEmail(timestamp, email):
+    sendAddress = app.config['EMAIL_ID']
+    sendPwd = app.config['EMAIL_PWD']
+    # context = ssl.create_default_context()
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            # smtp.starttls(context=context)
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(sendAddress, sendPwd)
+            subject = 'Test if email sends'
+            body = f'sent!'
+            msg = f'Subject: {subject}\n\n{body}'
+            smtp.sendmail(sendAddress, email, msg)
+            print('email sent')
--- app/views.py
+++ app/views.py
@@ -1,31 +1,49 @@
-from flask import Flask, redirect, url_for, render_template
-from app import forms
+from flask import Flask, redirect, url_for, render_template, request
+# from app import forms
 from app import tools
 from werkzeug.utils import secure_filename
 import os
 from app import app
 from flask.helpers import flash
+from datetime import datetime
 import shutil
 import smtplib
 # import locAL3 as lc3
 # import xAndyPlot as xyPlot
 
-
-# app.config['SECRET_KEY'] = '\xd8\xf1\xa5\xdd\x8eD\xf7\xdf]\xe7\x05\xf79\xa3\x0e\xd1'
-# uploads_dir = os.path.join(app.instance_path, 'uploads')
-# os.makedirs(uploads_dir, exist_ok=True)
-
-# Bootstrap(app)
+# home page -> render index.html
 @app.route("/") 
 def home():
     return render_template("index.html", title = "HOME", id = "home")
 
+# link for consult
+# upload and run code for consult
 @app.route("/consult", methods=('GET', 'POST')) 
 def consult():
-    # return getForm("consult")
+    if request.method == 'POST':
+        # if file does not exist in request.files
+        if 'file' not in request.files:
+            print("is it here?")
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if no uploaded file
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowedFile(file.filename):
+            timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+            filename = secure_filename(file.filename)
+            uploaded = os.path.join(app.config['IMAGE_UPLOADS'], timestamp)
+            file.save(uploaded)
+            email = request.form.get('userEmail')
+            tools.sendEmail(timestamp, email)
+            return redirect(url_for('pending'))
+            # return uploaded_file(filename)
     return render_template("consult.html", title = "CONSULT", id = "consult")
     
-
+# link for respect
+# upload and run code for respect
 @app.route("/respect", methods=('GET', 'POST')) 
 def respect():
     # return getForm("respect")
@@ -56,7 +74,11 @@ def getForm(currentPg):
         seqFile.save(os.path.join(uploads_dir, fileName))
         flash('Document uploaded successfully!')
         return redirect("pending.html")
-    return render_template(htmlLink, title = currentPg.upper(), id = currentPg, form = form)
+    return render_template(htmlLink, title = currentPg.upper(), id = currentPg, form = form)\
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# return true or false
+def allowedFile(filename):
+    # if the filename has '.' and the word after the '.' 
+    # is in allowedExtension -> return true
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
