3dd9e654e345df585d3c10627c84b1494dfd1359
Hyo Kim <hik010@ucsd.edu>
2021-03-04 15:19:29 -0800

    added skmer reference command

--- app/views.py
+++ app/views.py
@@ -55,7 +55,7 @@ def respect():
 	if request.method == 'POST':
 		if 'folder' not in request.files:
 			print("is it here?")
-			flash('No file part')
+			flash('No directory')
 			return redirect(request.url)
 		timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
 		print(request.files.getlist('folder'))
@@ -63,8 +63,16 @@ def respect():
 
 		hasHist = False
 		hasMapping = False
+		
+		#making the input and output directory 
+		input_dir = os.path.join(app.config['IMAGE_UPLOADS'],'respect',timestamp)
+		if not os.path.exists(input_dir):
+			os.makedirs(input_dir,exist_ok=True)
+		output_dir = os.path.join(app.config['IMAGE_UPLOADS'],'respect',timestamp+'_results')
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir,exist_ok=True)
 
-
+		#for all input files in the directory
 		for file in request.files.getlist('folder'):
 			# if no uploaded file
 			if file.filename == '':
@@ -72,13 +80,12 @@ def respect():
 				return redirect(request.url)
 			#check for file format
 			if file and allowedFile(file.filename):
-				if not os.path.exists(timestamp):
-					os.makedirs(os.path.join(app.config['IMAGE_UPLOADS'],timestamp),exist_ok=True)
+				
 				#checking if there's a histogram file
 				if file.filename.rsplit('.',1)[1].lower() == 'hist':
 					hasHist = True
 				filename = secure_filename(file.filename)
-				uploaded = os.path.join(app.config['IMAGE_UPLOADS'],timestamp, filename)
+				uploaded = os.path.join(input_dir, filename)
 				file.save(uploaded)
 				print(filename)
 			# if extension is not acceptable
@@ -101,13 +108,14 @@ def respect():
 			# if there is an input file and the file extension is acceptable
 			if h_file and allowedFile(h_file.filename):
 				hist_info_filename = secure_filename(h_file.filename)
-				uploaded = os.path.join(app.config['IMAGE_UPLOADS'],timestamp, hist_info_filename)
-				h_file.save(uploaded)
+				hist_file_path = os.path.join(input_dir, hist_info_filename)
+				h_file.save(hist_file_path)
 				print(hist_info_filename + ' saved as histogram info file')
 			# if exension is not acceptable
 			else:
 				flash("Unacceptable extension. Only accept: {}".format(ext + "and .gz version"), 'warning')
 				return redirect(request.url)
+
 		##for mapping file
 		if 'mapping-file' in request.files:
 			m_file = request.files['mapping-file']
@@ -115,79 +123,118 @@ def respect():
 			if m_file.filename != '' and allowedFile(m_file.filename):
 				hasMapping = True
 				mapping_filename = secure_filename(m_file.filename)
-				uploaded = os.path.join(app.config['IMAGE_UPLOADS'],timestamp, mapping_filename)
-				m_file.save(uploaded)
+				mapping_file_path = os.path.join(input_dir, mapping_filename)
+				m_file.save(mapping_file_path)
 				print(mapping_filename + ' saved as mapping file')
 			# if file format is not accepted
 			elif m_file.filename != '':
 				flash("Unacceptable extension. Only accept: {}".format(ext + " and .gz version"), 'warning')
 				return redirect(request.url)
-		#making outputs folder
-		if not os.path.exists(timestamp+'_results'):
-			os.makedirs(os.path.join(app.config['IMAGE_UPLOADS'],timestamp+'_results'),exist_ok=True)
-		
-		input_dir = os.path.join(app.config['IMAGE_UPLOADS'],timestamp)
-		output_dir = os.path.join(app.config['IMAGE_UPLOADS'],timestamp+'_results/')
 
 		#passing over to shell command
 		if((hasHist == False) and (hasMapping == False)):
 			c = "respect -d {0} -N 10 --debug -o {1}".format(input_dir,output_dir)
 		elif ((hasHist == True) and (hasMapping == False)): 
-			c = "respect -d {0} -I {1} -N 10 --debug -o {2}".format(input_dir,input_dir+'/'+hist_info_filename, output_dir)
+			c = "respect -d {0} -I {1} -N 10 --debug -o {2}".format(input_dir,hist_file_path, output_dir)
 		elif ((hasHist == False) and (hasMapping == True)): 
-			c = "respect -d {0} -m {1} -N 10 --debug -o {2}".format(input_dir,input_dir+'/'+mapping_filename, output_dir)
+			c = "respect -d {0} -m {1} -N 10 --debug -o {2}".format(input_dir,mapping_file_path, output_dir)
 		else: 
-			c = "respect -d {0} -m {1} -I {2} -N 10 --debug -o {3}".format(input_dir,input_dir+'/'+mapping_filename, input_dir+'/'+hist_info_filename ,output_dir)
+			c = "respect -d {0} -m {1} -I {2} -N 10 --debug -o {3}".format(input_dir,mapping_file_path, hist_file_path,output_dir)
 		
 		#runs respect
+		run_command(c)
 		flash("Successfully Uploaded Files! This might take a while. You can safely exit out of this page", "info")
-		run_respect(c)
 		# send email when the respect is done running
 		email = request.form.get('userEmail')
-		getResultdir = timestamp+'_results'
-		tools.sendEmail(timestamp, email, output_dir, getResultdir)
+		getResultdir = 'respect/'+timestamp+'_results'
+		# tools.sendEmail(timestamp, email, output_dir, getResultdir)
 		
 		return redirect(url_for("result", result_dir = getResultdir))
-		# return render_template("results.html", title = "RESULT", id = "result", name = output_dir)
-		# return redirect(url_for('run_respect', command=c))
-		# return redirect(url_for('pending'))
-		# return redirect(url_for('download_file', name=filename))
+
 	return render_template("respect.html", title = "RESPECT", id = "respect")
 
+	
+# link for skmer
+# upload and run code for respect
+@app.route("/skmer", methods=('GET', 'POST')) 
+def skmer():
+	ext = "txt, csv, hist, fa, fq, fastq, fna, fasta"
+	if request.method == 'POST':
+		if 'folder' not in request.files:
+			print("is it here?")
+			flash('No directory')
+			return redirect(request.url)
+		timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+		print(request.files.getlist('folder'))
+
+		#making the input and output directory 
+		input_dir = os.path.join(app.config['IMAGE_UPLOADS'],'skmer',timestamp)
+		if not os.path.exists(input_dir):
+			os.makedirs(input_dir,exist_ok=True)
+		output_dir = os.path.join(app.config['IMAGE_UPLOADS'],'skmer',timestamp+'_results')
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir,exist_ok=True)
+
+		for file in request.files.getlist('folder'):
+			# if no uploaded file
+			if file.filename == '':
+				flash('No selected file')
+				return redirect(request.url)
+			#check for file format
+			if file and allowedFile(file.filename):
+				#uploads the files to uploads/skmer
+				filename = secure_filename(file.filename)
+				uploaded = os.path.join(input_dir, filename)
+				file.save(uploaded)
+				print(filename)
+				# if extension is not acceptable
+			else:
+				flash("Unacceptable extension. Only accept: {}".format(ext + "and .gz version"), 'warning')
+				return redirect(request.url)
+
+		librarydir = os.path.join(output_dir,'processed_library')
+		dist_matrix_prefix = os.path.join(output_dir,'ref-dist-mat')
+		c = "skmer reference {0} -l {1} -o {2} ".format(input_dir,librarydir,dist_matrix_prefix)
+		
+		#runs skmer 
+		run_command(c)
+		flash("Successfully Uploaded Files! This might take a while. You can safely exit out of this page", "info")
+		# send email when the respect is done running
+		email = request.form.get('userEmail')
+		getResultdir = 'skmer/'+timestamp+'_results'
+		# tools.sendEmail(timestamp, email, output_dir, getResultdir)
+		return redirect(url_for("result", result_dir = getResultdir))
+
+
+	return render_template("skmer.html", title = "SKMER", id = "skmer")
+
+
 # generate a result url -> display after done running a tool
-@app.route("/result/<result_dir>")
+@app.route("/result/<path:result_dir>")
 def result(result_dir):
 	# link = url_for('result', result_dir=result_dir, _external= True)
 	# tools.sendEmail(timestamp, email, output_dir, result_dir)]
 	output_dir = os.path.join(app.config['IMAGE_UPLOADS'], result_dir)
 	return render_template("results.html", title = "RESULT", id = "result", name = output_dir + "/")
 
-def run_respect(command):
-	# return 'Hello World'
-	try:
-		result = subprocess.check_output([command],shell=True)
-		return result
-	except subprocess.CalledProcessError as e:
-		print( "error occurred")
-		flash('Sub Process Error')
-		return str(e)
-	
+
+#make and return zip file	
 @app.route("/return_files/<path:dirname>", methods=('GET', 'POST'))
 def return_files(dirname):
 	try:
 		#getting absolute path to results folder 
 		results_dir = os.path.join(os.getcwd(), dirname)
-		zip_path = os.path.join(results_dir,'output.zip')
-
-		zipf = zipfile.ZipFile(zip_path,'w', zipfile.ZIP_DEFLATED)
-		files = os.listdir(results_dir)
-		for file in files:
-			zipf.write(dirname+file)
-
+		
+		# zip_path = os.path.join(results_dir,'output.zip')
+		# zipf = zipfile.ZipFile(zip_path,'w', zipfile.ZIP_DEFLATED)
+		# files = os.listdir(results_dir)
 		# for root,dirs,files in os.walk(results_dir):
 		# 	for file in files:
 		# 		zipf.write(dirname+file)
-		zipf.close()
+		# zipf.close()
+
+		shutil.make_archive(os.path.join(results_dir,'output'), 'zip', results_dir)
+
 		return send_from_directory(directory = results_dir,filename='output.zip',
 				mimetype = 'zip',
 				attachment_filename= 'output.zip',
@@ -225,9 +272,6 @@ def getForm(currentPg):
 		return redirect("pending.html")
 	return render_template(htmlLink, title = currentPg.upper(), id = currentPg, form = form)\
 
-
-
-
 # return true or false
 def allowedFile(filename):
 	# if the filename has '.' and the word after the '.' 
@@ -246,3 +290,13 @@ def allowedFile(filename):
 	# '.' in filename and \
 	# 	(filename.rsplit('.',1)[1].lower() in app.config['ALLOWED_EXTENSIONS']) \
 	# 		# or filename.rsplit('.')[1].lower() + ".gz" in app.config['ALLOWED_EXTENSIONS'] )
+
+def run_command(command):
+	# return 'Hello World'
+	try:
+		result = subprocess.check_output([command],shell=True)
+		return result
+	except subprocess.CalledProcessError as e:
+		print( "error occurred")
+		flash('Sub Process Error')
+		return str(e)
